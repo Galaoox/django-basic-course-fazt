8b57a157980e34dd39996ea43bd178c5087a29b7
Erick Vergara <erickandres30@gmail.com>
2023-05-24 22:28:23 -0500

    django admin

--- apps/myapp/admin.py
+++ apps/myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Project, Task
 
-# Register your models here.
+# Solo usando el admin.site.register se puede ver los modelos en el admin de django
+admin.site.register(Project)
+admin.site.register(Task)
--- apps/myapp/models.py
+++ apps/myapp/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
@@ -13,3 +16,6 @@ class Task(models.Model):
     completed = models.BooleanField(default=False)
     # de esta forma se hacen las relaciones entre tablas y se usa la eliminacion en cascada
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title + " - " + self.project.name
