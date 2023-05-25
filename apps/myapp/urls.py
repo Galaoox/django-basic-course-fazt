9490b441d867b5c0c70f98fdd88f54a5598d07ee
Erick Vergara <erickandres30@gmail.com>
2023-05-24 22:18:42 -0500

    operation of database queries

--- apps/myapp/urls.py
+++ apps/myapp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import hello, about, index
+from .views import hello, about, index, projects, tasks
 
 urlpatterns = [
     path('', index),
     path('about/', about),
     # de esta forma se pasan parametros a la url, se debe definir el tipo del parametro
     path('hello/<str:username>', hello),
+    path('projects/', projects),
+    path('tasks/<int:id>', tasks),
+
 ]
--- apps/myapp/views.py
+++ apps/myapp/views.py
@@ -1,5 +1,8 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
 
@@ -14,3 +17,14 @@ def hello(request, username):  # de esta forma se reciben los parametros que vie
 
 def about(request):
     return HttpResponse("<h1> About page ! </h1>")
+
+
+def projects(request):
+    projects = list(Project.objects.all().values())
+    return JsonResponse(projects, safe=False)
+
+
+def tasks(request, id):
+    # se puede usar estas funciones de django para obtener un objeto o un 404
+    task = get_object_or_404(Task, id=id)
+    return HttpResponse(f'<h1> Task: {task.title} </h1>')
